adea1d3f1c9c93086f97c03f56a9ec9b5e3c0629
Brian Randell <octobrian@github.com>
2022-10-13 15:13:27 +0200

    Update home.js
    Add Rust as a language.

--- src/components/home.js
+++ src/components/home.js
@@ -3,7 +3,7 @@ const PropTypes = require('prop-types');
 const api = require('../utils/api');
 
 function SelectLanguage(props) { 
-  const languages = ['All', 'JavaScript', 'Java', 'CSharp', 'Python', 'Go'];
+  const languages = ['All', 'JavaScript', 'Java', 'CSharp', 'Python', 'Go', 'Rust'];
 
   return (
     <div>
